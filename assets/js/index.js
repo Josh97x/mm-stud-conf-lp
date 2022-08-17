b76e3a4fa10f38bb9d4a86747629778160a8d565
97th Floor <97thfloor@97ths-MacBook-Pro.local>
2022-08-17 11:44:34 -0600

    calculated time for days, hours, minutes and seconds

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,4 +10,9 @@ let timeInterval = setInterval(function () {
   let currentTime = new Date().getTime();
 
   let timeLeft = countdownDate - currentTime;
+
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 });
