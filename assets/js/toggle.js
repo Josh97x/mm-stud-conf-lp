5bffb4f3982208a301af7e3ff620c9db74d30f23
97th Floor <97thfloor@97ths-MacBook-Pro.local>
2022-08-24 13:45:11 -0600

    edited nav classnames / hid index.js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,11 +1,11 @@
 // logic for NAV TOGGLE
 function classToggle() {
-  const navs = document.querySelectorAll(".Navbar__Items");
+  const navs = document.querySelectorAll(".navbar-items");
   console.log(navs);
 
-  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+  navs.forEach((nav) => nav.classList.toggle("navbar-toggleShow"));
 }
 
 document
-  .querySelector(".Navbar__Link-toggle")
+  .querySelector(".navbar-link-toggle")
   .addEventListener("click", classToggle);
