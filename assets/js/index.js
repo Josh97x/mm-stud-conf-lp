1e6d238d4eab1494b2787974635e01674b7b1949
97th Floor <97thfloor@97ths-MacBook-Pro.local>
2022-08-24 13:26:57 -0600

    added temp-new nav with js/css

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,3 +33,15 @@ let timeInterval = setInterval(function () {
     clock.innerHTML = "EXPIRED";
   }
 }, 1000);
+
+// logic for NAV TOGGLE
+function classToggle() {
+  const navs = document.querySelectorAll(".Navbar__Items");
+  console.log(navs);
+
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+}
+
+document
+  .querySelector(".Navbar__Link-toggle")
+  .addEventListener("click", classToggle);
